b90026abba9360e76a32030b907dfdcafafdbd6f
BoyFriendSoulEvil <108184484+BoyFriendSoulEvil@users.noreply.github.com>
2022-07-28 23:09:48 -0300

    Add files via upload

---
+++ assets/preload/data/sleeptalk/SongName__Port.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Port by BoyFriendSoulEvil |", 0, 25, 500); -- lua by BoyFriendSoulEvil
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
